7855de53a0c02bfef080f9b7208b5ccd2f3b085f
meet2aman <meet2amankushwaha@gmail.com>
2023-11-26 19:50:58 +0530

    cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+d;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      console.log("Error Uploading could't find file Path");
+      return null;
+    }
+    // upload the file on cloudinary
+    cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been successfully uploaded
+    console.log("file is uploaded sucessfully on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    // remove the locally saved temporary file as the upload operation failed
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
