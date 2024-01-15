d94416b016f9bbd2d6113438da929d7df7ec6f1c
meet2aman <meet2amankushwaha@gmail.com>
2024-01-15 12:56:16 +0530

    models updated

---
+++ src/models/comment.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+    },
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
